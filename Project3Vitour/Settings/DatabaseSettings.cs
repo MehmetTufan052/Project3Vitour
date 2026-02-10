fcc3c282368418bc380e1bf5928755afaa086857
MehmetTufan052 <tufaneser8@gmail.com>
2026-02-10 22:14:30 +0300

    Add project files.

---
+++ Project3Vitour/Settings/DatabaseSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace Project3Vitour.Settings
+{
+    public class DatabaseSettings:IDatabaseSettings
+    {
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+        public string TourCollectionName { get; set; }
+    }
+}
