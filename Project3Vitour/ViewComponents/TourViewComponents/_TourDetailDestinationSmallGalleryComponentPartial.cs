6550302e3a2ba9df07393ac1358e631bb8f74e02
MehmetTufan052 <tufaneser8@gmail.com>
2026-03-04 05:01:53 +0300

    update

--- Project3Vitour/ViewComponents/TourViewComponents/_TourDetailDestinationSmallGalleryComponentPartial.cs
+++ Project3Vitour/ViewComponents/TourViewComponents/_TourDetailDestinationSmallGalleryComponentPartial.cs
@@ -1,12 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Project3Vitour.Services.GalleryService;
 
 namespace Project3Vitour.ViewComponents.TourViewComponents
 {
     public class _TourDetailDestinationSmallGalleryComponentPartial:ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IGalleryService _galleryService;
+
+        public _TourDetailDestinationSmallGalleryComponentPartial(IGalleryService galleryService)
         {
-            return View();
+            _galleryService = galleryService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var values = await _galleryService.GetAllGalleryAsync();
+            return View(values);
+          
         }
     }
 }
