6755f14ef7cbb921d113264608bb5bfb1624cbe0
MehmetTufan052 <tufaneser8@gmail.com>
2026-03-05 17:11:12 +0300

    update

---
+++ Project3Vitour/ViewComponents/TourViewComponents/_TourDetailLast3TourComponentPartial.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Project3Vitour.Services.TourPlanService;
+using Project3Vitour.Services.TourServices.ITourService;
+
+namespace Project3Vitour.ViewComponents.TourViewComponents
+{
+    public class _TourDetailLast3TourComponentPartial : ViewComponent
+    {
+        ITourService _tourService;
+
+        public _TourDetailLast3TourComponentPartial(ITourService tourService)
+        {
+            _tourService = tourService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var values = await _tourService.GetAllTourAsync();
+            var last3 = values.TakeLast(3).ToList();
+            return View(last3);
+        }
+    }
+}
---
+++ Project3Vitour/ViewComponents/TourViewComponents/_TourDetailTourPlaningComponentPartial.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Project3Vitour.Entities;
+using Project3Vitour.Services.TourPlanService;
+using Project3Vitour.Services.TourServices.ITourService;
+
+namespace Project3Vitour.ViewComponents.TourViewComponents
+{
+    public class _TourDetailTourPlaningComponentPartial : ViewComponent
+    {
+        ITourPlanService _tourPlanService;
+
+        public _TourDetailTourPlaningComponentPartial(ITourPlanService tourPlanService)
+        {
+            _tourPlanService = tourPlanService;
+        }
+
+        public async Task< IViewComponentResult> InvokeAsync(string tourId)
+        {
+            var values = await _tourPlanService.GetAllTourPlanAsync();
+            var filtered = values.Where(x => x.TourId == tourId).ToList();
+            return View(filtered);
+        }
+    }
+}
