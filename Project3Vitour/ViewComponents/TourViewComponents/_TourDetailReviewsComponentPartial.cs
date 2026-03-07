05f75ee710f50074569a9e2a86f5bac0190a09c2
MehmetTufan052 <tufaneser8@gmail.com>
2026-03-07 04:01:10 +0300

    update

---
+++ Project3Vitour/ViewComponents/TourViewComponents/_TourDetailReviewsComponentPartial.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Project3Vitour.Entities;
+using Project3Vitour.Services.ReviewService;
+using Project3Vitour.Services.TourServices.ITourService;
+
+namespace Project3Vitour.ViewComponents.TourViewComponents
+{
+    public class _TourDetailReviewsComponentPartial : ViewComponent
+    {
+        IReviewService _reviewService;
+
+        public _TourDetailReviewsComponentPartial(IReviewService reviewService)
+        {
+            _reviewService = reviewService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(string tourId)
+        {
+            var values = await _reviewService.GetAllReviewAsync();
+            var filtered = values.Where(x => x.TourId == tourId).ToList();
+            return View(filtered);
+        }
+    }
+}
