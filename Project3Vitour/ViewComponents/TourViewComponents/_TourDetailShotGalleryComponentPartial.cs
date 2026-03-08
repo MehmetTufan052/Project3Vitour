24abd504cc9843c72aaadfec0a045a4d4ee02ea9
MehmetTufan052 <tufaneser8@gmail.com>
2026-03-08 03:31:44 +0300

    ...

---
+++ Project3Vitour/ViewComponents/TourViewComponents/_TourDetailShotGalleryComponentPartial.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Project3Vitour.Services.GalleryService;
+
+namespace Project3Vitour.ViewComponents.TourViewComponents
+{
+    public class _TourDetailShotGalleryComponentPartial : ViewComponent
+    {
+        private readonly IGalleryService _galleryService;
+
+        public _TourDetailShotGalleryComponentPartial(IGalleryService galleryService)
+        {
+            _galleryService = galleryService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(string tourId)
+        {
+            var values = await _galleryService.GetAllGalleryAsync();
+            var filtered = values.Where(x => x.TourId == tourId).ToList();
+            return View(filtered);
+        }
+    }
+}
