5902aa5b8406497b3b29973efce77e28228b8c01
MehmetTufan052 <tufaneser8@gmail.com>
2026-03-25 23:41:09 +0300

    ...

--- Project3Vitour/ViewComponents/TourViewComponents/_TourFooterComponentPartial.cs
+++ Project3Vitour/ViewComponents/TourViewComponents/_TourFooterComponentPartial.cs
@@ -1,12 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Project3Vitour.Services.GalleryService;
 
 namespace Project3Vitour.ViewComponents.TourViewComponents
 {
     public class _TourFooterComponentPartial:ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IGalleryService _galleryService;
+
+        public _TourFooterComponentPartial(IGalleryService galleryService)
         {
-            return View();
+            _galleryService = galleryService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var galleryList=await _galleryService.GetAllGalleryAsync();
+            var top6 = galleryList.Take(6).ToList();
+            return View(top6);
         }
     }
 }
